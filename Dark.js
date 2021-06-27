34204dd152a5e1dde2863ae56efa9335a3693e93
Super_Fusiom <paul21479@gmail.com>
2021-06-27 22:34:57 +1200

    Themes removed due to no use and hard to manage

--- Dark.js
+++ Dark.js
@@ -1,18 +1,3 @@
-
-// themes for the website
-function swapStyleSheet(sheet) {
-    document.getElementById("pagestyle").setAttribute("href", sheet);
-}
-
-function initate() {
-    var style1 = document.getElementById("Dark");
-    var style2 = document.getElementById("Light");
-
-    style1.onclick = function () { swapStyleSheet("css/Dark.css"); };
-    style2.onclick = function () { swapStyleSheet("css/Light.css"); };
-}
-
-window.onload = initate;
 // Sidebar opening and closing
 function sideopen() {
     document.getElementById("Sidebar").style.width = "250px";
